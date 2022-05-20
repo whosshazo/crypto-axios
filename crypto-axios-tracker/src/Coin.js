c02b90c060148a0f68cb1f8552e8de9ca55728fe
whosshazo <ryandeshazo1@gmail.com>
2022-05-20 12:00:31 -0400

    styling for page

--- crypto-axios-tracker/src/Coin.js
+++ crypto-axios-tracker/src/Coin.js
@@ -1,6 +1,15 @@
 import React from "react";
+import "./Coin.css";
 
-const Coin = ({ image, name, symbol, price, volume, priceChange, marketcap }) => {
+const Coin = ({
+  image,
+  name,
+  symbol,
+  price,
+  volume,
+  priceChange,
+  marketcap,
+}) => {
   return (
     <div className="coin-container">
       <div className="coin-row">
