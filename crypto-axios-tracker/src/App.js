4651395442edbca21f2fd09b9c78b37920d90402
whosshazo <ryandeshazo1@gmail.com>
2022-05-20 10:05:06 -0400

    setting up API and catch error for API

--- crypto-axios-tracker/src/App.js
+++ crypto-axios-tracker/src/App.js
@@ -1,6 +1,21 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      )
+      .then((res) => {
+        setCoins(res.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <div className="App">
       <h1>Hello World</h1>
